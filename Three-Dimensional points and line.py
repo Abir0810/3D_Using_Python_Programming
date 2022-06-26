c9caf447c590dd70a6764964c0aacad6a24eea7a
Abir Abdullha <65417150+Abir0810@users.noreply.github.com>
2022-06-26 13:40:41 -0500

    Add files via upload

---
+++ Three-Dimensional points and line.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Three-Dimensional points and line
+
+# In[3]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+ax=plt.axes(projection='3d')
+z=np.linspace(0,15,1000)
+x=np.sin(z)
+y=np.cos(z)
+ax.plot3D(x,y,z, 'red')
+z= 15*np.random.random(100)
+x=np.sin(z)+0.1*np.random.randn(100)
+y=np.cos(z)+0.1*np.random.randn(100)
+ax.scatter3D(x,y,z, c=z,cmap='Greens')
+plt.show()
+
+
+# In[ ]:
+
+
+
+
